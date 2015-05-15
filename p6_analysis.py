9a94c792fad7e8cfb89e8a76b934d362439f9d32
Joe Wise <JosephWise93@gmail.com>
2015-05-15 04:12:37 -0700

    implemented BFS

--- p6_analysis.py
+++ p6_analysis.py
@@ -4,15 +4,22 @@ ANALYSIS = {}
 
 def analyze(design):
     sim = Simulator(design)
-	init = sim.get_initial_state()
-	moves = sim.get_moves()
-	next_state = sim.get_next_state(init, moves[0])
+    init = sim.get_initial_state()
+    moves = sim.get_moves()
+    # next_state = sim.get_next_state(init, moves[0])
+    # position, abilities = next_state # or None if character dies
+    # i, j = position
 
-	position, abilities = next_state # or None if character dies
-	i, j = position
+    ANALYSIS[init] = None
+    q = [init]
+    while q:
+        node = q.pop()
 
-	#insert RIKLS here
-
+        neighbors = [sim.get_next_state(node, move) for move in moves]
+        for next_node in neighbors:
+            if next_node not in ANALYSIS:
+                ANALYSIS[next_node] = node
+                q.append(next_node)
 
 def inspect((i,j), draw_line):
     # TODO: use ANALYSIS and (i,j) draw some lines
