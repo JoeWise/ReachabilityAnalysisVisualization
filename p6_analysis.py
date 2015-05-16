23f2178d2b25bfe6ae9d7cb7c9ba01bee06258c2
Joe Wise <JosephWise93@gmail.com>
2015-05-15 21:18:09 -0700

    reimplemented BFS. Implemented line drawing. Added custom level.

--- p6_analysis.py
+++ p6_analysis.py
@@ -1,26 +1,56 @@
 from p6_game import Simulator
+import p6_tool
 
 ANALYSIS = {}
 
 def analyze(design):
     sim = Simulator(design)
     init = sim.get_initial_state()
-    moves = sim.get_moves()
     # next_state = sim.get_next_state(init, moves[0])
     # position, abilities = next_state # or None if character dies
     # i, j = position
 
-    ANALYSIS[init] = None
+    prev = {init: None}
     q = [init]
     while q:
-        node = q.pop()
+        curr_state = q.pop(0)
+        # print (str(curr_state[0]))
 
-        neighbors = [sim.get_next_state(node, move) for move in moves]
-        for next_node in neighbors:
-            if next_node not in ANALYSIS:
-                ANALYSIS[next_node] = node
-                q.append(next_node)
+        moves = sim.get_moves()
+        neighbors = []
+        for move in moves:
+            neighbor = sim.get_next_state(curr_state, move)
+            if neighbor is not None:
+                neighbors.append(neighbor)
 
-def inspect((i,j), draw_line):
+
+        for next_state in neighbors:
+            if next_state not in prev:
+                prev[next_state] = curr_state
+                q.append(next_state)
+
+    ANALYSIS['prev'] = prev
+    print('analysis complete')
+
+def inspect((i, j), draw_line):
     # TODO: use ANALYSIS and (i,j) draw some lines
-    raise NotImplementedError
+    pos = (i, j)
+    prev = ANALYSIS['prev']
+
+    # for state in points[pos]:
+    #     prev_state = prev[state]
+    #     curr_state = state
+    #     while prev_state:
+    #         draw_line(curr_state[0], prev_state[0])
+    #         curr_state = prev_state
+    #         prev_state = prev[prev_state]
+    for state in prev:
+        if state[0] == pos:
+            prev_state = prev[state]
+            curr_state = state
+            offset = p6_tool.make_offset()
+            color = p6_tool.make_color()
+            while prev_state:
+                draw_line(curr_state[0], prev_state[0], offset, color)
+                curr_state = prev_state
+                prev_state = prev[prev_state]
